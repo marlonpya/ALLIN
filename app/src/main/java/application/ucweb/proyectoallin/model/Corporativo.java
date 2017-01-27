fb029238cf1c6b7dcc7c634e501dae111f6879bf
Sheylavc <sheylavc@hotmail.com>
2017-01-27 11:33:46 -0500

    20-01-2017 11:33

--- app/src/main/java/application/ucweb/proyectoallin/model/Corporativo.java
+++ app/src/main/java/application/ucweb/proyectoallin/model/Corporativo.java
@@ -14,27 +14,63 @@ public class Corporativo extends RealmObject {
     private static final String TAG = Corporativo.class.getSimpleName();
     private static final String ID = "id";
     public static final int ID_DEFAULT = 1;
+    public static final int ID_NOT_FOUND = -1;
 
     @PrimaryKey
     private long id;
     private int id_server;
+    private int id_local;
+    private int id_evento;
     private String nombre;
     private boolean sesion;
+    private int loc_id;
+    private String loc_nombre;
 
-    public void iniciarSesion(Corporativo corporativo) {
+    public static void iniciarSesion(Corporativo corporativo) {
         Realm realm = Realm.getDefaultInstance();
         Corporativo select_corporativo = realm.where(Corporativo.class).equalTo(ID, ID_DEFAULT).findFirst();
         realm.beginTransaction();
         if (select_corporativo == null) {
-            Corporativo insert_corporativo = realm.createObject(Corporativo.class, corporativo.getId());
+            Corporativo insert_corporativo = realm.createObject(Corporativo.class, ID_DEFAULT);
             insert_corporativo.setId_server(corporativo.getId_server());
+            insert_corporativo.setId_local(corporativo.getId_local());
+            insert_corporativo.setId_evento(corporativo.getId_evento());
             insert_corporativo.setNombre(corporativo.getNombre());
             insert_corporativo.setSesion(true);
-            realm.copyToRealm(corporativo);
+            insert_corporativo.setLoc_id(corporativo.getLoc_id());
+            insert_corporativo.setLoc_nombre(corporativo.getLoc_nombre());
+            realm.copyToRealm(insert_corporativo);
             Log.d(TAG, insert_corporativo.toString());
         } else {
-
+            select_corporativo.setId_server(corporativo.getId_server());
+            select_corporativo.setId_local(corporativo.getId_local());
+            select_corporativo.setId_evento(corporativo.getId_evento());
+            select_corporativo.setNombre(corporativo.getNombre());
+            select_corporativo.setSesion(true);
+            select_corporativo.setLoc_id(corporativo.getLoc_id());
+            select_corporativo.setLoc_nombre(corporativo.getLoc_nombre());
+            Log.d(TAG, select_corporativo.toString());
         }
+        realm.commitTransaction();
+        realm.close();
+    }
+
+    public static void cerrarSesion() {
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+        Corporativo corporativo = realm.where(Corporativo.class).equalTo(ID, ID_DEFAULT).findFirst();
+        corporativo.setId_server(-1);
+        corporativo.setNombre("");
+        corporativo.setSesion(false);
+        corporativo.setLoc_id(-1);
+        corporativo.setLoc_nombre("");
+        realm.commitTransaction();
+        realm.close();
+    }
+
+    public static Corporativo getCorporativo() {
+        Realm realm = Realm.getDefaultInstance();
+        return realm.where(Corporativo.class).equalTo(ID, ID_DEFAULT).findFirst();
     }
 
     public long getId() {
@@ -53,6 +89,22 @@ public class Corporativo extends RealmObject {
         this.id_server = id_server;
     }
 
+    public int getId_local() {
+        return id_local;
+    }
+
+    public void setId_local(int id_local) {
+        this.id_local = id_local;
+    }
+
+    public int getId_evento() {
+        return id_evento;
+    }
+
+    public void setId_evento(int id_evento) {
+        this.id_evento = id_evento;
+    }
+
     public String getNombre() {
         return nombre;
     }
@@ -67,5 +119,21 @@ public class Corporativo extends RealmObject {
 
     public void setSesion(boolean sesion) {
         this.sesion = sesion;
+    }
+
+    public int getLoc_id() {
+        return loc_id;
+    }
+
+    public void setLoc_id(int loc_id) {
+        this.loc_id = loc_id;
+    }
+
+    public String getLoc_nombre() {
+        return loc_nombre;
+    }
+
+    public void setLoc_nombre(String loc_nombre) {
+        this.loc_nombre = loc_nombre;
     }
 }
